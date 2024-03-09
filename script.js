22c459018f81facd9d217dac446c5e564a28f765
interniums <intuition228@gmail.com>
2024-03-09 11:42:20 +0200

    1

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 let firstNumber = NaN;
 let secondNumber = NaN;
-let operator = '  ';
+let operator = '';
 let output = '';
+let output2 = '';
 
 function display(_inputValue) {
 	const display = document.getElementById('span')
@@ -9,12 +10,9 @@ function display(_inputValue) {
 	display.textContent = output;
 }
 
-function result() {
-	
-}
-
 const number_7 = function () {
-	output+= '7'
+	display(7);
+	return firstNumber = 7;
 }
 
 const number_8 = function () {
@@ -29,5 +27,5 @@ const number_9 = function () {
 
 const divide = function() {
 	display(' / ');
-	return operator = ' / '; 
+	return operator = '/'; 
 }
